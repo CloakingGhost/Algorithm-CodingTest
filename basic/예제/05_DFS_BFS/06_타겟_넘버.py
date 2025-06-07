74626c7f25eb908daecff2032037fbe2d53359d9
JaeHyeon_Lee <project0858@gmail.com>
2025-06-07 23:15:30 +0900

    [TIL]새로운 유형의 DFS, BFS
    [타겟 넘버]
    주어진 정점만큼 반복하면 됨

--- basic/예제/05_DFS_BFS/06_타겟_넘버.py
+++ basic/예제/05_DFS_BFS/06_타겟_넘버.py
@@ -1,5 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
+# region
+
 
 def solution(numbers, target):
     def dfs(depth, total):
@@ -14,6 +16,43 @@ def solution(numbers, target):
 
     answer = 0
     dfs(0, 0)
+    return answer
+
+
+# endregion
+
+
+def solution(numbers, target):
+    answer = 0
+
+    def dfs(depth, total):
+        if depth == len(numbers):
+            if total == target:
+                nonlocal answer
+                answer += 1
+            return
+        dfs(depth + 1, total + numbers[depth])
+        dfs(depth + 1, total - numbers[depth])
+
+    dfs(0, 0)
+    return answer
+
+
+from collections import deque
+
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque([(0, 0)])
+    while queue:
+        depth, total = queue.popleft()
+        if depth == len(numbers):
+            if total == target:
+                answer += 1
+            continue
+
+        queue.append((depth + 1, total + numbers[depth]))
+        queue.append((depth + 1, total - numbers[depth]))
     return answer
 
 
