19cd65c4cc7ebfce924a000cc36ceef4286aedfc
JaeHyeon_Lee <project0858@gmail.com>
2025-06-19 11:23:09 +0900

    til: 트리
    반례를 고민해야 하는 문제
    - 구현이 전부가 아니라 정확한 상황파악이 우선

--- basic/예제/07_트리/02_트리.py
+++ basic/예제/07_트리/02_트리.py
@@ -1,46 +1,52 @@
 # https://www.acmicpc.net/problem/1068
 import sys
 
-LEFT, RIGHT, EMPTY = 0, 1, -1
+EMPTY = -1
 input = sys.stdin.readline
 n = int(input())
-tree = [[EMPTY, EMPTY] for _ in range(n)]
-nodes = list(map(int, input().split()))
-for node in range(1, n):
-    parent = nodes[node]
-    if node % 2:
-        tree[parent][LEFT] = node
-    else:
-        tree[parent][RIGHT] = node
-
+tree = [[] for _ in range(n)]
+parents = list(map(int, input().split()))
 delete_node = int(input())
+root = None
+for node in range(n):
+    parent = parents[node]
 
-parent = delete_node // 2
-if tree[parent][LEFT] == delete_node:
-    tree[parent][LEFT] = EMPTY
-else:
-    tree[parent][RIGHT] = EMPTY
+    if parent == EMPTY:
+        root = node
+        continue
+
+    if delete_node == node:
+        continue
+
+    tree[parent].append(node)
+
 
 # 탐색
-count = 0
 
 
-def preorder(center):
-    global count
-    if tree[center][LEFT] == EMPTY and tree[center][RIGHT] == EMPTY:
-        count += 1
-        return
+def dfs(parent):
+    if not tree[parent]:
+        return 1
 
-    if tree[center][LEFT] != EMPTY:
-        preorder(tree[center][LEFT])
+    count = 0
 
-    if tree[center][RIGHT] != EMPTY:
-        preorder(tree[center][RIGHT])
+    for next_node in tree[parent]:
+        count += dfs(next_node)
+    return count
 
 
-if delete_node == 0:
+if delete_node == root:
     print(0)
 else:
-    preorder(0)
+    print(dfs(root))
 
-    print(count)
+
+"""
+6
+-1 0 0 0 1 1
+2
+
+5
+0 0 1 1 -1
+2
+"""
